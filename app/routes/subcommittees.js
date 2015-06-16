fd930a935fa401125ee8f44d444cbfca321f3ea0
Alec Arme & James Robert Williams <student@epicodus.com>
2015-06-16 14:10:52 -0700

    Add subcommitties outlet

--- app/routes/subcommittees.js
+++ app/routes/subcommittees.js
@@ -5,8 +5,9 @@ export default Ember.Route.extend({
     var url = "https://congress.api.sunlightfoundation.com/committees?subcommittee=true&parent_committee_id=" + params.committee_id + "&apikey=634cb825aab141b7991dca9fbe5c4523";
     return Ember.$.getJSON(url).then(function(responseJSON) {
       var subcommittees = [];
-      responseJSON.results.forEach(function(committee) {
-        subcommittees.push(committee);
+      responseJSON.results.forEach(function(subcommittee) {
+        subcommittees.push(subcommittee);
+      debugger;
       });
       return subcommittees;
     });
